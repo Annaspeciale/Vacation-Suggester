f72ca65f54f5ffbd1ab455e441b07c5d82dd3dc6
Anna Matviyenko <anna.matviyenko@gmail.com>
2016-06-10 14:28:57 -0700

    Initiate branching in js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
-$(document).ready(function) {
-	var question1 = 
+$(document).ready(function() {
+	$("#blanks form").submit(function(event) {
+		var answer1 = $("inpit#cuisine").val();
+		var answer2 = $("input#activity").val();
+		var answer3 = $("input#clothes").val();
+		var answer4 = $("input#season").val();
+		var answer5 = $("input#personality").val();
+
+
+if (answer1 === 'Mexican' && answer2 === 'Laying on the beach' && answer4 === 'Summer') {
+	$('mexico').show();
 }
 
-if 
+});
+});
