8be581daa68a71b0c1be6ac8345c2021c712807f
Anna Matviyenko <anna.matviyenko@gmail.com>
2016-06-20 14:41:36 -0700

    Add final fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("#blanks form").submit(function(event) {
-		var answer1 = $("inpit#cuisine").val();
+		var answer1 = $("input#cuisine").val();
 		var answer2 = $("input#activity").val();
 		var answer3 = $("input#clothes").val();
 		var answer4 = $("input#season").val();
